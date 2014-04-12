3cb046603ef72e60a972a79d1ccf920c741f7ddf
Dan Erickson <druckson@gmail.com>
2014-04-12 13:16:23 -0700

    change default port to 5000

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res) {
     res.send("Hello World!");
 });
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
