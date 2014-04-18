0eb94bd745ad5908d9f659903612f06114473815
Dan Erickson <druckson@gmail.com>
2014-04-18 16:57:07 -0700

    add connect-jade-html for partials

--- app/server.js
+++ app/server.js
@@ -1,23 +1,32 @@
 var express = require("express");
 var stylish = require("stylish");
+var connect_jade_html = require("connect-jade-html");
 var app = express();
 
 app.set('views', __dirname + "/views/");
 app.set('view engine', "jade");
 app.use(stylish(__dirname + "/public/"));
+app.use(connect_jade_html({
+    src: __dirname + "/views/",
+    dest: __dirname + "/public/"
+}));
 app.use(express.static(__dirname + "/public/"));
 
 app.get('/', function(req, res) {
     res.render('index', {});
 });
 
-app.get('/main.html', function(req, res) {
-    res.render('partials/main', {});
-});
-
-app.get('/main.html', function(req, res) {
-    res.render('partials/main', {});
-});
+//app.get('/partials/main.html', function(req, res) {
+//    res.render('partials/main', {});
+//});
+//
+//app.get('/partials/skills.html', function(req, res) {
+//    res.render('partials/skills', {});
+//});
+//
+//app.get('/partials/experience.html', function(req, res) {
+//    res.render('partials/experience', {});
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
